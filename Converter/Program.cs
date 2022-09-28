7580afc50afb7f196617513535082619a2e7af74
lora-tuzova <114598459+lora-tuzova@users.noreply.github.com>
2022-09-28 21:49:13 +0300

    Update Program.cs

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -1,9 +1,17 @@
-﻿Console.WriteLine("Type in the current price of USD in UAH: ");
-decimal u = decimal.Parse(Console.ReadLine());
-Console.WriteLine("Type in the current price of EUR in UAH: ");
-decimal e = decimal.Parse(Console.ReadLine());
-Converter converter = new Converter(u, e);
-converter.ToConvert();
+Console.WriteLine("Type in the current price of USD in UAH: ");
+try
+{
+    decimal u = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Type in the current price of EUR in UAH: ");
+    decimal e = decimal.Parse(Console.ReadLine());
+    Converter converter = new Converter(u, e);
+    converter.ToConvert();
+}
+catch
+{
+    Console.WriteLine("Format of price is incorrect");
+}
+
 
 class Converter
 {
@@ -16,9 +24,16 @@ class Converter
     }
     public void ToConvert()
     {
-        Console.WriteLine("Type in the amount of money in UAH: ");
-        decimal UAH = decimal.Parse(Console.ReadLine());
-        Console.WriteLine($"Amount in USD: {UAH / USDtoUAH}\n");
-        Console.WriteLine($"Amount in EUR: {UAH / EURtoUAH}\n");
+        try
+        {
+            Console.WriteLine("Type in the amount of money in UAH: ");
+            decimal UAH = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"Amount in USD: {UAH / USDtoUAH}\n");
+            Console.WriteLine($"Amount in EUR: {UAH / EURtoUAH}\n");
+        }
+        catch
+        {
+            Console.WriteLine("Format of amount is incorrect");
+        }
     }
 }
