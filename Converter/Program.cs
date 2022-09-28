606ba7eed8c7dbc13558a348753559d0ca3261f7
Admin <Admin@DESKTOP-SN4C7E6>
2022-09-28 21:41:06 +0300

    Добавьте файлы проекта.

---
+++ Converter/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Type in the current price of USD in UAH: ");
+decimal u = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Type in the current price of EUR in UAH: ");
+decimal e = decimal.Parse(Console.ReadLine());
+Converter converter = new Converter(u, e);
+converter.ToConvert();
+
+class Converter
+{
+    decimal USDtoUAH;
+    decimal EURtoUAH;
+    public Converter (decimal USD, decimal EUR)
+    {
+        USDtoUAH = USD;
+        EURtoUAH = EUR;
+    }
+    public void ToConvert()
+    {
+        Console.WriteLine("Type in the amount of money in UAH: ");
+        decimal UAH = decimal.Parse(Console.ReadLine());
+        Console.WriteLine($"Amount in USD: {UAH / USDtoUAH}\n");
+        Console.WriteLine($"Amount in EUR: {UAH / EURtoUAH}\n");
+    }
+}
